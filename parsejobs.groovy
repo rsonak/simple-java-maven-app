eb81a64ce8cc34749d1cb04cad266a1f55a07c5f
Rohit Sonak <rohit.sonak@Rohit-Sonak-MacBook-Pro.local>
2018-02-22 14:37:57 -0800

    Updated jenkinsfile

--- parsejobs.groovy
+++ parsejobs.groovy
@@ -3,7 +3,7 @@ println "start parser"
 
 def getJobNameReposMap() {
     def jsonSlurper = new JsonSlurper()
-    def data = jsonSlurper.parseText(new File("/Users/rohit.sonak/Documents/GitHub/simple-java-maven-app/buildjobs.json").text)
+    def data = jsonSlurper.parseText(new File("./buildjobs.json").text)
     def itemDataMap = [:]
     data.jobs.each { 
     	def jobName = it["name"]
