cdb5c1f5217c0ce80487d43070f1a84900d10800
Rohit Sonak <rohit.sonak@Jobvites-MacBook-Pro-3.local>
2018-03-08 15:36:23 -0800

    updated

--- parsejobs.groovy
+++ parsejobs.groovy
@@ -3,27 +3,23 @@ import groovy.json.*
 def getJobNameReposMap() {
 	println "start parser" 
     def data = parseFile()
-    println("Data = "+data)
+    println("Data")
     def itemDataMap = [:]
-    data.each { 
-    	def jobName = "jobs".it["name"]
+    data.jobs.each { 
+    	def jobName = it["name"]
     	//def jobRepos = it["repos"].location
-    	def jobRepos = "jobs".it["repos"]
+    	def jobRepos = it["repos"]
     	itemDataMap.put(jobName,jobRepos)
     }
     println "end parser" 
     return itemDataMap  
 }
 
-@NonCPS
 def parseFile() {
 	println "start parsing file"
 	def jsonSlurper = new JsonSlurper()
 	def jsonFile = readFile("./buildjobs.json")
 	def object = jsonSlurper.parseText(jsonFile)
-	if(object instanceof groovy.json.internal.LazyMap) {
-      return new HashMap<>(object)
-  	}
   	println "end parsing file"
 	return object
 }
